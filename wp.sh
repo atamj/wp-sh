ecbba2c2ddf82613e1b47cbf8fc492017297785f
Jael <jael.atam971@gmail.com>
2019-08-17 19:43:27 -0400

    Maj 1

--- wp.sh
+++ wp.sh
@@ -3,26 +3,45 @@ clear
 echo "============================================"
 echo "WordPress Install Script"
 echo "============================================"
+echo "Nom du projet:"
+read -e project
 echo "Database Name: "
 read -e dbname
 echo "Database User: "
 read -e dbuser
 echo "Database Password: "
 read -s dbpass
+echo "Avez-vous déjà créer un BD pour ce projet ? (y/n)"
+read -e bdExiste
+if [ "$bdExiste" == n ]; then
+	echo "Créer la BD avec 1-Mysql(natif), 2-Mysql(MAMP) ou 0-Aucun ? (1, 2 ou 0) "
+	read -e mysql
+	if [ "$mysql" == 1 ]; 
+	then
+		mysql --host=localhost -u"$dbuser" -p"$dbpass" -e "CREATE DATABASE $dbname;"
+	elif [ "$mysql" == 2 ]; 
+	then
+		/Applications/MAMP/Library/bin/mysql --host=localhost -u"$dbuser" -p"$dbpass" -e "CREATE DATABASE $dbname;"
+	else
+		exit
+	fi
+fi
 echo "run install? (y/n)"
 read -e run
 if [ "$run" == n ] ; then
 exit
 else
-echo "============================================"
+echo "=========================================================="
 echo "Installation de la dernière version de WordPress."
-echo "============================================"
+echo "=========================================================="
 #download wordpress
 curl -O https://wordpress.org/latest.tar.gz
 #unzip wordpress
 tar -zxvf latest.tar.gz
-#change dir to wordpress
-cd wordpress
+#rename folder project
+mv wordpress/ "$project"
+#change dir to wordpress project
+cd "$project"
 #create wp config
 echo "Enregistrement de la configuration."
 cp wp-config-sample.php wp-config.php
@@ -59,11 +78,29 @@ cd ..
 cd ..
 cd ..
 rm latest.tar.gz
+#deplacer le projet
+mv "$project" ../
+cd ..
 #remove bash script
-rm wp.sh
+echo "Voulez-vous supprimer le script wp.sh ?(y/n)"
+read -e rmScript
+if [ "$remScript" == y ]; then
+	rm -rf wp-sh
+fi
 echo "========================="
 echo "Installation is complete."
 echo "========================="
-cd wordpress/wp-content/themes/base_wp_underscores
+echo "Lancer sublime text ? (y/n)"
+read -e sublime
+cd "$project"/wp-content/themes/base_wp_underscores
+if [ "$sublime" == y ]; then
+	subl .
+fi
+if [[ "$OSTYPE" == "darwin"*  ]]; 
+then
+	open http://localhost/"$project"
+else
+	xdg-open http://localhost/"$project"
+fi
 yarn watch
 fi
