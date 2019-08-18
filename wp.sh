500748ab606801a37b49903ff6f95e4899fb4604
Jael <jael.atam971@gmail.com>
2019-08-18 17:34:27 -0400

    Ajout d'interraction et d'activation de thème

--- wp.sh
+++ wp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 clear
-echo "============================================"
-echo "WordPress Install Script"
-echo "============================================"
+echo "==============================================="
+echo "	WordPress Install Script by Jaël"
+echo "==============================================="
 echo "Nom du projet:"
 read -e project
 echo "Database Name: "
@@ -14,6 +14,7 @@ read -s dbpass
 echo "Avez-vous déjà créer un BD pour ce projet ? (y/n)"
 read -e bdExiste
 if [ "$bdExiste" == n ]; then
+	#création de la bd
 	echo "Créer la BD avec 1-Mysql(natif), 2-Mysql(MAMP) ou 0-Aucun ? (1, 2 ou 0) "
 	read -e mysql
 	if [ "$mysql" == 1 ]; 
@@ -32,7 +33,7 @@ if [ "$run" == n ] ; then
 exit
 else
 echo "=========================================================="
-echo "Installation de la dernière version de WordPress."
+echo "	Installation de la dernière version de WordPress."
 echo "=========================================================="
 #download wordpress
 curl -O https://wordpress.org/latest.tar.gz
@@ -42,30 +43,49 @@ tar -zxvf latest.tar.gz
 mv wordpress/ "$project"
 #change dir to wordpress project
 cd "$project"
-#create wp config
-echo "Enregistrement de la configuration."
-cp wp-config-sample.php wp-config.php
-#set database details with perl find and replace
-perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
-perl -pi -e "s/username_here/$dbuser/g" wp-config.php
-perl -pi -e "s/password_here/$dbpass/g" wp-config.php
+echo ""
+echo " 'y' si vous avez WP-CLI et que vous voulez l'utiliser (conseillé) sinon 'n' "
+read -e wpcli
+if [[ "$wpcli" == y ]]; then
+	echo "Admin user:"
+	read -e adminUser
+	echo "Admin password:"
+	read -s adminPass
+	echo "Admin email:"
+	read -e adminEmail
+	wp core config --dbname="$dbname" --dbuser="$dbuser" --dbpass="$dbpass" --dbhost=localhost --dbprefix="$dbname"_
+	wp core install --url=http://localhost/"$project" --title="$project" --admin_user="$adminUser" --admin_password="$adminPass" --admin_email="$adminEmail"
+else
+	"Création du wp-config.php ..."
+	#create wp config
+	cp wp-config-sample.php wp-config.php
+	echo "Enregistrement de la configuration..."
+	#set database details with perl find and replace
+	perl -pi -e "s/database_name_here/$dbname/g" wp-config.php
+	perl -pi -e "s/username_here/$dbuser/g" wp-config.php
+	perl -pi -e "s/password_here/$dbpass/g" wp-config.php
+	#set WP salts
+	perl -i -pe'
+	  BEGIN {
+	    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
+	    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
+	    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
+	  }
+	  s/put your unique phrase here/salt()/ge
+	' wp-config.php
+fi
 
-#set WP salts
-perl -i -pe'
-  BEGIN {
-    @chars = ("a" .. "z", "A" .. "Z", 0 .. 9);
-    push @chars, split //, "!@#$%^&*()-_ []{}<>~\`+=,.;:/?|";
-    sub salt { join "", map $chars[ rand @chars ], 1 .. 64 }
-  }
-  s/put your unique phrase here/salt()/ge
-' wp-config.php
 #Installation du thème
 echo "Téléchargement du thème."
-#Deplacement dans le dossier wp-content
-cd wp-content
 #Déplacement dans le dossier thème
-cd themes
+cd wp-content/themes
+#Téléchargement du thème
 git clone https://github.com/atamj/base_wp_underscores.git
+if [[ "$wpcli" == y ]]; then
+	cd ../..
+	wp theme activate base_wp_underscores
+	cd wp-content/themes
+fi
 echo "Yarn install"
 cd base_wp_underscores
 yarn install
@@ -87,20 +107,27 @@ read -e rmScript
 if [ "$remScript" == y ]; then
 	rm -rf wp-sh
 fi
+echo "Ouvrir votre navigateur par défaut à la fin de l'installation ? (y/n)"
+read -e navigateur
 echo "========================="
 echo "Installation is complete."
 echo "========================="
 echo "Lancer sublime text ? (y/n)"
 read -e sublime
-cd "$project"/wp-content/themes/base_wp_underscores
 if [ "$sublime" == y ]; then
+	cd "$project"
 	subl .
-fi
-if [[ "$OSTYPE" == "darwin"*  ]]; 
-then
-	open http://localhost/"$project"
+	cd /wp-content/themes/base_wp_underscores
 else
-	xdg-open http://localhost/"$project"
+	cd "$project"/wp-content/themes/base_wp_underscores
+fi
+if [[ "$navigateur" == y ]]; then
+	if [[ "$OSTYPE" == "darwin"*  ]]; 
+	then
+		open http://localhost/"$project"
+	else
+		xdg-open http://localhost/"$project"
+	fi
 fi
 yarn watch
 fi
